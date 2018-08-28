c1abedae537cbbf9feafd1ef04a15713c7c34540
Baguvix72 <efanov.gag.48@gmail.com>
2018-08-28 15:23:38 +0300

    Use state in card component

--- src/Card.js
+++ src/Card.js
@@ -3,24 +3,46 @@ import ModalWindow from './ModalWindow';
 import color from './DataColor';
 
 class Card extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      WindowVisible: true 
+    }
+  }
+
   render() {
     const {srvc} = this.props;
+    const Visible = this.state.WindowVisible;
+    const cardHeader = Visible ? <h5 class="card-title">{srvc.name}</h5> : null;
+
     return (
       <div class={color[this.getRandomInt(0, color.length - 1)]}>
         <div class="card-header">
-        <h5 class="card-title">{srvc.name}</h5>
+            <div className="text-left">
+              <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" onClick={this.changeVisible}/>
+            </div>
+        {cardHeader}
+        {/* <h5 class="card-title">{srvc.name}</h5> */}
         </div>
         <div class="card-body">
           <p class="card-text">{srvc.discription}</p>
         </div>
         <div class="card-footer text-muted">
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter" >
             Мне это интересно!
           </button>
         </div>
         <ModalWindow />
       </div>
     );
+  }
+
+  changeVisible = () => {
+    this.setState({
+      WindowVisible: !this.state.WindowVisible
+    })
   }
 
   getRandomInt(min, max) {
